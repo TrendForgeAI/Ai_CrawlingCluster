6db994a8fc449002a66cc7eca25ec147e401ab58
sky81219 <=>
2024-12-19 12:20:51 +0900

    산업-영향력키워드 대한민국-redis적재

--- databases/keyword_generator.py
+++ databases/keyword_generator.py
@@ -1,24 +1,20 @@
 from pathlib import Path
+from typing import TypedDict
+from dataclasses import dataclass
 import itertools
 import yaml
 
 
+@dataclass(frozen=True)
 class BaseCountry:
     """
     국가별 키워드 및 템플릿 데이터를 관리하는 클래스.
     """
 
-    def __init__(
-        self,
-        name: str,
-        core_keywords: list[str],
-        context_keywords: list[str],
-        templates: list[str],
-    ):
-        self.name = name
-        self.core_keywords = core_keywords
-        self.context_keywords = context_keywords
-        self.templates = templates
+    name: str
+    core_keywords: list[str]
+    context_keywords: list[str]
+    templates: list[str]
 
     def generate_search_queries(self) -> list[tuple[str, str]]:
         """
@@ -48,9 +44,9 @@ def load_countries_from_yaml() -> dict[str, BaseCountry]:
     """
     keyword_yaml_path = Path(__file__).parent.parent / "configs/cy/keyword.yaml"
     with open(keyword_yaml_path, "r", encoding="utf-8") as file:
-        data = yaml.safe_load(file)
+        data: dict = yaml.safe_load(file)
 
-    countries = {
+    countries: dict[str, BaseCountry] = {
         country_code: BaseCountry(
             name=country_code,
             core_keywords=country_data["core_keywords"],
--- keyword_gener.py
+++ keyword_gener.py
@@ -1,23 +1,28 @@
+import json
 from databases.keyword_generator import BaseCountry, load_countries_from_yaml
+from databases.cache.redis_cluster_manager import RedisClusterManager
 
 
-def generate_questions_for_country(country: BaseCountry) -> None:
+def generate_korea_questions(country_code: str) -> set[str]:
     """
-    특정 국가에 대한 질문 생성 및 출력
+    한국에 대한 질문을 생성하고 반환하는 메인 함수
+    Returns:
+        한국 관련 생성된 질문 리스트
     """
-    queries = country.generate_search_queries()
-    questions = country.apply_question_templates(queries)
+    countries: dict[str, BaseCountry] = load_countries_from_yaml()
+    country: BaseCountry = countries[country_code]
+    return country.apply_question_templates(country.generate_search_queries())
 
-    return questions
+
+def save_redis_keyword(country_code: str) -> None:
+    """레디스 저장
+
+    Args:
+        country_code (str): 국가 코드
+    """
+    c: list[str] = list(generate_korea_questions(country_code))
+    manager = RedisClusterManager()
+    manager.store_data("KR:question", value=json.dumps(c, ensure_ascii=False))
 
 
-# 실행
-countries: dict[str, BaseCountry] = load_countries_from_yaml()
-
-# 대한민국 질문 생성
-korea = generate_questions_for_country(countries["KR"])
-
-# 미국 질문 생성
-us = generate_questions_for_country(countries["US"])
-
-print(korea)
+save_redis_keyword("KR")
